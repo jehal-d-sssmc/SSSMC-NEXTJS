58bf48e98aa8c6f4426a2388bb53aa65f379644a
jehal-d-sssmc <106730131+jehal-d-sssmc@users.noreply.github.com>
2023-08-03 18:45:49 +0530

    6 files modified, 4 added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,80 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
+import $ from 'jquery';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      jquery: false,
+    };
+  }
+  async componentDidMount() {
+    this.setState(
+      {
+        jquery: require('jquery'),
+      },
+      () => {
+        const $ = this.state.jquery;
+        $(document).ready(function () {
+          $('.js-menu-icon').on('click', function () {
+            $(this).toggleClass('fa-times fa-bars');
+            $('.menu .container').removeClass('container--is-visible');
+            $('.menu li').removeClass('is-selected');
+            $('.js-navbar').toggleClass('navbar--is-visible');
+          });
+
+          $('.menu li').on('click', function (e) {
+            e.preventDefault();
+            var $this = $(this);
+            $this.toggleClass('is-selected');
+
+            var $currentContainer = $(this).find('.container');
+            $currentContainer.toggleClass('container--is-visible');
+
+            $('.menu .container')
+              .not($currentContainer)
+              .removeClass('container--is-visible');
+            $('.menu li').not($this).removeClass('is-selected');
+          });
+        });
+      }
+    );
+  }
+
+  render() {
+    const Component = this.props.Component;
+    return (
+      <>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+            integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+          />
+        </Head>
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+          crossOrigin="anonymous"
+        />
+        <Component {...this.props} {...this.state} />
+      </>
+    );
+  }
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Header from '../client/components/Header';
+import SwiperComp from '../client/components/SwiperComp';
+import Shorts from '../client/components/Shorts';
 
 export default class Home extends React.Component {
   render() {
     return (
       <>
         <Header />
+        <div>
+          <SwiperComp />
+        </div>
+        <div className="p-3">
+          <h3 className="section-title">Shorts</h3>
+          <div className="p-2"></div>
+          <Shorts />
+        </div>
       </>
     );
   }
