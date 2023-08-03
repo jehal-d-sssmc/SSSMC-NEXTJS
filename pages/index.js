7796c7dcd09ac6d7f287f9b6138ab9c2b5801b6f
jehal-d-sssmc <106730131+jehal-d-sssmc@users.noreply.github.com>
2023-08-03 16:07:03 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Header from '../client/components/Header';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    );
+  }
+}
